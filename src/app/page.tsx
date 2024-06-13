d5f639a69e1b64f838d6809232102f075c9b6789
egwimcodes <egwimcodes@gmail.com>
2024-06-13 23:08:37 +0100

    fixing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,9 +57,9 @@ export default function Home() {
               // Ensure userData is a JSON string before storing
               const dataToStore = typeof e === 'string' ? e : JSON.stringify(e);
               await setItem("userData", dataToStore);
-              const accessToken = e.access;
+              const accessToken = e.token.access;
               await setItem("accessToken", accessToken);
-              alert(`from Register: ${JSON.stringify(e.access, null, 2)}`);
+              alert(`from Register: ${JSON.stringify(e.token.access, null, 2)}`);
 
               setUserDataHook(e);
             } catch (error) {
