ab1fc86374a187183aadc71cac2febc1dbe6d368
egwimcodes <egwimcodes@gmail.com>
2024-06-13 17:28:07 +0100

    fixing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,30 @@
 "use client"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import HomePage from "@/components/Pages/HomePage";
 import _404 from "@/components/Pages/_404";
+import { CgPassword } from "react-icons/cg";
+const myData = {
+  username: "Snowwisdom",
+  password: "1113269206",
+  first_name: "Wisdom",
+  referral_code: "",
+  isPremium_user: false,
 
+}
 export default function Home() {
   const [show404, setShow404] = useState(false);
 
+
+  useEffect(() => {
+    alert(JSON.stringify(myData))
+  })
   return (
     <>
       {show404 ? (
         <_404 />
       ) : (
           <HomePage />     
-    
       )
       }
     </>
