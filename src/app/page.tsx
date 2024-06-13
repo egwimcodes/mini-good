1ccba0135121562e47ed169989f2740c93a9c813
egwimcodes <egwimcodes@gmail.com>
2024-06-13 19:52:44 +0100

    fixing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,52 @@
 "use client"
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from "react";
-import HomePage from "@/components/Pages/HomePage";
+import { useCloudStorage, useWebApp } from "@vkruglikov/react-telegram-web-app";
 import _404 from "@/components/Pages/_404";
-import { CgPassword } from "react-icons/cg";
 import { Register } from "@/utils/requests";
-import { UserData } from "@/types";
-import { useWebApp, useCloudStorage } from "@vkruglikov/react-telegram-web-app";
 import { UserContext } from "@/hooks/UserContext";
-
+import { UserData } from "@/types";
+import HomePage from "@/components/Pages/HomePage";
+// import { MyLocalStorage } from "./localStorage/localStorage";
 
 export default function Home() {
-  let { getItem, setItem } = useCloudStorage();
+  const { setItem, getItem } = useCloudStorage()
   const [show404, setShow404] = useState(false);
   const [userDataHook, setUserDataHook] = useState<UserData>();
-  const webApp = useWebApp();
+  //TODO: create usestate to store registered user data
+
+  const data = useWebApp();
+
+  useEffect(() => {
+    const handleContextMenu = (event: MouseEvent) => {
+      event.preventDefault();
+    };
+
+    document.addEventListener(
+      "contextmenu",
+      handleContextMenu
+    );
+
+    return () => {
+      document.removeEventListener(
+        "contextmenu",
+        handleContextMenu
+      );
+    };
+  }, []);
 
   useEffect(() => {
     if (
-      webApp.platform &&
-      (webApp.platform === "unknown" ||
-        webApp.platform === "tdesktop")
+      data.platform &&
+      (data.platform === "unknown" ||
+        data.platform === "tdesktop")
     ) {
       setShow404(true);
       return;
     }
-    webApp.expand();
-    const userData = webApp.initDataUnsafe;
+    data.expand();
+    const userData = data.initDataUnsafe;
     const userInfo = {
       password: `${userData.user.id}`,
       username: userData.user.username,
@@ -43,14 +63,6 @@ export default function Home() {
             const dataToStore = typeof e === 'string' ? e : JSON.stringify(e);
             await setItem('userLocalData', dataToStore);
 
-            const userData = await getItem('userLocalData');
-
-            if (userData !== null) {
-              setUserDataHook(JSON.parse(userData));
-            }
-
-            alert(`Data stored successfully: ${JSON.stringify(userDataHook)}`);
-
           } catch (error) {
             alert(`Error storing data: ${error}`);
           }
@@ -58,24 +70,45 @@ export default function Home() {
         storeData();
       })
       .catch(
-        (e) => alert(`Error from Register ${JSON.stringify(e)}`));
-  }, [webApp]);
+        (e) => alert(`Error from Register ${JSON.stringify(e)}`)
+      );
+
+    const getStoreData = async () => {
+      try {
+        const storedData = await getItem('userLocalData');
+        if (storedData) {
+          const parsedValue = JSON.parse(storedData);
+          setUserDataHook(parsedValue);
+
+          // alert(`Stored data from Register: ${JSON.stringify(parsedValue, null, 2)}`);
+        }
+      }
+      catch (error) {
+        alert(`Error fetching data from localStorage: ${error}`);
+      }
+    }
+
+    getStoreData();
+  }, [data, setItem, getItem]);
 
   return (
     <>
       {show404 ? (
         <_404 />
-      ) : (userDataHook !== null && (
-        webApp.platform &&
-        (webApp.platform === "android" ||
-          webApp.platform === "ios") &&
-        webApp.initDataUnsafe &&
+      ) : (
+        // userDataHook !== null && (
+        // webApp.platform &&
+        // (webApp.platform === "android" ||
+        //   webApp.platform === "ios") &&
+        // webApp.initDataUnsafe &&
         (
           <UserContext.Provider value={userDataHook}>
             <HomePage />
           </UserContext.Provider>
         )
-      ))}
+        // ))}
+      )
+}
     </>
   );
 }
