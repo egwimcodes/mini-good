ba85d9f250e6fbf6240c86ef8fdaeb1e0b9a40bb
egwimcodes <egwimcodes@gmail.com>
2024-06-14 19:11:40 +0100

    fixing

--- src/components/Pages/Referral.tsx
+++ src/components/Pages/Referral.tsx
@@ -16,7 +16,7 @@ export default function Referral() {
               <div className="ref-body w-[95%] h-[30%]">
                   <div className="invite-content-container h-[100%] flex flex-col justify-center items-center">
                       <div className="cont-container w-full h-[100%] flex flex-row justify-between">
-                          <div className="invite-cont w-[49%] h-[100%] flex rounded-[10px] flex-col items-start justify-between p-2  border-2 border-orange-400 ">
+                          <div className="invite-cont w-[49%] h-[100%] flex rounded-[10px] flex-col items-start justify-between p-2  border-2 border-main ">
                               <h3 className="invite-h-text h-[20%] text-base sm:text-base md:text-lg lg:text-xl xl:text-2xl font-semibold">Invite friends</h3>
 
                               <div className="coin-text h-[40%] flex flex-row items-center">
@@ -24,7 +24,7 @@ export default function Referral() {
                                   <h3 className="invite-text text-base sm:text-base md:text-lg lg:text-xl xl:text-2xl ml-1 font-bold text-orange-400">+2000</h3>
                               </div>
                           </div>
-                          <div className="invite-cont w-[49%] h-[100%] flex rounded-[10px] flex-col items-start justify-between p-2  border-2 border-orange-400 ">
+                          <div className="invite-cont w-[49%] h-[100%] flex rounded-[10px] flex-col items-start justify-between p-2  border-2 border-main ">
                               <h3 className="invite-h-text h-[20%] text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl font-semibold">Invite friends with <br /> premium</h3>
 
                               <div className="coin-text h-[40%] flex flex-row items-center">
@@ -40,7 +40,7 @@ export default function Referral() {
               </div>
 
               <div className="ref-btn-container h-[50%] flex flex-col items-center justify-between ">
-                  <div className="my-referals w-[100%] h-[80%] p-5 border-2 border-orange-400 rounded-[30px]">
+                  <div className="my-referals w-[100%] h-[80%] p-5 border-2 border-main rounded-[30px]">
                       <div className="referal-item">
                           <p className="text-light text-xl font-bold">MY REFERALS <span className="text-2xl text-purple-600">0</span></p>
                           <div className="referal-head"></div>
@@ -50,7 +50,7 @@ export default function Referral() {
                       </div>
 
                   </div>
-                  <div className="referal-btn flex w-[95vw] h-[18%] flex-col justify-center items-center border-2 border-orange-400 rounded-[20px]">
+                  <div className="referal-btn flex w-[95vw] h-[18%] flex-col justify-center items-center border-2 border-main rounded-[20px]">
                       <p>INIVITE FRIENDS</p>
                   </div>
               </div>
