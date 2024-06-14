6b7428a742af7d89646eace1b85d4923addf7a99
egwimcodes <egwimcodes@gmail.com>
2024-06-14 19:19:08 +0100

    fixing

--- src/components/Pages/Profile.tsx
+++ src/components/Pages/Profile.tsx
@@ -14,7 +14,7 @@ export default function Profile({ setShowProfile }: ProfileProps) {
     return (
         <div className='w-[100vw] h-[90vh] text-light flex-col ' onClick={() => setShowProfile("profile")}>
             <div className="profile-content w-[95vw] h-[30vh] flex-col flex-center">
-                <div className="avater-profile w-[20vw] border-2 border-orange-400 rounded-[50%]">
+                <div className="avater-profile w-[20vw] border-2 border-main rounded-[50%]">
                     <Image src="/robot.png" width={100} height={100} alt="" />
                 </div>
                 <p className='text-3xl font-bold'>{user.username}</p>
@@ -22,35 +22,35 @@ export default function Profile({ setShowProfile }: ProfileProps) {
                     {/* <ChargeLevel level={100} availableGoodCoin={100} chargedGoodCoin={100} /> */}
                     <div className="level flex flex-row items-center justify-center flex-nowrap">
                         <Image className="w-8" width={100} height={100} src='/regendcup.png' draggable="false" alt="" />
-                        <p className="text-base text-gray font-bold text-orange-400 mx-1">{user.rank}</p>
+                        <p className="text-base text-gray font-bold text-main mx-1">{user.rank}</p>
                         <MdNavigateNext className="text-xl text-gray text-slate-400" />
                     </div>
                 </div>
             </div>
-            <hr className="w-[95vw] border-1 border-orange-400 mx-auto" />
+            <hr className="w-[95vw] border-1 border-main mx-auto" />
             <div className="profile-content w-[95vw] h-[60vh] flex-col flex-center">
                 <div className="profile-contnt-items flex-col flex-center">
                     <h4 className="text-1xl font-bold">Daily Earners</h4>
                     <div className="children-body flex flex-row items-center justify-center w-full" >
                         <Image className="w-5" width={50} height={50} src='/goodcoing.png' draggable="false" alt="" />
-                        <p className="text-base text-gray font-bold text-orange-400 mx-1">900 MIllion</p>
+                        <p className="text-base text-gray font-bold text-main mx-1">900 MIllion</p>
                     </div>
                 </div>
-                <hr className="w-[70vw] border-1 my-3 border-orange-400 mx-auto" />
+                <hr className="w-[70vw] border-1 my-3 border-main mx-auto" />
                 <div className="profile-contnt-items flex-col flex-center">
                     <h4 className="text-1xl font-bold">Active</h4>
                     <div className="children-body flex flex-row items-center justify-center w-full" >
-                        <p className="text-base text-gray font-bold text-orange-400 mx-1">2455675</p>
+                        <p className="text-base text-gray font-bold text-main mx-1">2455675</p>
                     </div>
                 </div>
-                <hr className="w-[50vw] border-1 my-3 border-orange-400 mx-auto" />
+                <hr className="w-[50vw] border-1 my-3 border-main mx-auto" />
                 <div className="profile-contnt-items flex-col flex-center">
                     <h4 className="text-1xl font-bold">All Taps</h4>
                     <div className="children-body flex flex-row items-center justify-center w-full" >
-                        <p className="text-base text-gray font-bold text-orange-400 mx-1">100M</p>
+                        <p className="text-base text-gray font-bold text-main mx-1">100M</p>
                     </div>
                 </div>
-                <hr className="w-[30vw] border-1 my-3 border-orange-400 mx-auto" />
+                <hr className="w-[30vw] border-1 my-3 border-main mx-auto" />
 
             </div>
         </div>
--- src/components/Pages/Wallet.tsx
+++ src/components/Pages/Wallet.tsx
@@ -15,15 +15,15 @@ export default function Wallet({ setShowProfile }: WalletProps) {
                   <div className="children-body xsm:flex-2 flex-col items-center justify-center w-full" >
                       {/* <ChargeLevel level={100} availableGoodCoin={100} chargedGoodCoin={100} /> */}
                       <div className="level flex flex-row items-center justify-center flex-nowrap">
-                          <p className="text-base text-gray font-bold text-orange-400 mx-1">0.00 USDT</p>
+                          <p className="text-base text-gray font-bold text-main mx-1">0.00 USDT</p>
                       </div>
-                      <div className="credit w-[50vw] mx-auto mt-1 flex-row flex-center h-[5vh] bg-orange-400 rounded-3xl">
+                      <div className="credit w-[50vw] mx-auto mt-1 flex-row flex-center h-[5vh] bg-main rounded-3xl">
                           <h3>Connect Wallet</h3>
                       </div>
                       
                   </div>
               </div>
-              <hr className="w-[95vw] border-1 border-orange-400 mx-auto" />
+              <hr className="w-[95vw] border-1 border-main mx-auto" />
               <div className="credit  mx-auto flex-col flex-center h-[10vh] ">
                   <h3 className='text-1xl font-semibold'>Credit Your Wallet</h3>
                   <p className='text-xxxs font-semibold'>creadit your goodcoin wallet</p>
@@ -33,29 +33,29 @@ export default function Wallet({ setShowProfile }: WalletProps) {
                       <h4 className="text-1xl font-bold">USDT</h4>
                       <div className="children-body flex flex-row items-center justify-evenly w-[95vw]" >
                           <Image className="w-5" width={50} height={50} src='/usdt.png' draggable="false" alt="" />
-                          <p className="text-xxxs text-gray font-bold text-orange-400 mx-1">1A1zP1eP5QGe***</p>
+                          <p className="text-xxxs text-gray font-bold text-main mx-1">1A1zP1eP5QGe***</p>
                           <BsCopy className='text-xl text-gray text-slate-400'/>
                       </div>
                   </div>
-                  <hr className="w-[70vw] border-1 my-3 border-orange-400 mx-auto" />
+                  <hr className="w-[70vw] border-1 my-3 border-main mx-auto" />
                   <div className="profile-contnt-items flex-col flex-center">
                       <h4 className="text-1xl font-bold">USDT</h4>
                       <div className="children-body flex flex-row items-center justify-evenly w-[95vw]" >
                           <Image className="w-5" width={50} height={50} src='/usdt.png' draggable="false" alt="" />
-                          <p className="text-xxxs text-gray font-bold text-orange-400 mx-1">1A1zP1eP5QGe***</p>
+                          <p className="text-xxxs text-gray font-bold text-main mx-1">1A1zP1eP5QGe***</p>
                           <BsCopy className='text-xl text-gray text-slate-400' />
                       </div>
                   </div>
-                  <hr className="w-[70vw] border-1 my-3 border-orange-400 mx-auto" />
+                  <hr className="w-[70vw] border-1 my-3 border-main mx-auto" />
                   <div className="profile-contnt-items flex-col flex-center">
                       <h4 className="text-1xl font-bold">USDT</h4>
                       <div className="children-body flex flex-row items-center justify-evenly w-[95vw]" >
                           <Image className="w-5" width={50} height={50} src='/usdt.png' draggable="false" alt="" />
-                          <p className="text-xxxs text-gray font-bold text-orange-400 mx-1">1A1zP1eP5QGe***</p>
+                          <p className="text-xxxs text-gray font-bold text-main mx-1">1A1zP1eP5QGe***</p>
                           <BsCopy className='text-xl text-gray text-slate-400' />
                       </div>
                   </div>
-                  <hr className="w-[70vw] border-1 my-3 border-orange-400 mx-auto" />
+                  <hr className="w-[70vw] border-1 my-3 border-main mx-auto" />
               </div>
 
         </div>
