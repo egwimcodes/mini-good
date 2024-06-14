bb1005c80e42dafe6674b4ded769d612f6e2cec9
egwimcodes <egwimcodes@gmail.com>
2024-06-14 01:33:10 +0100

    fixing

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -3,9 +3,17 @@ import { MakeRequest } from "./axiosCall";
 import { AuthRoutes } from "./authEndpoints";
 import { Auth } from "../types";
 
+interface RequestConfig {
+    path: string;
+    method: string;
+    contentType: string;
+    removeAuth: boolean;
+    data: Auth;
+}
+
 async function Register({ password, username, first_name, is_premium_user, referral_code }: Auth) {
     try {
-        const requestConfig = {
+        const requestConfig: RequestConfig = {
             path: AuthRoutes.REGISTER,
             method: "POST",
             contentType: "application/json",
@@ -21,12 +29,12 @@ async function Register({ password, username, first_name, is_premium_user, refer
         const response = await MakeRequest(requestConfig);
         return response;
     } catch (error) {
-        throw error;
+        alert("Error during registration: " + error); // Using alert to show error message
+        console.error("Error during registration:", error);
+        throw error; // Optional: Re-throw the error to be handled by the caller
     }
 }
 
-
-
 export {
-    Register, 
+    Register,
 }
