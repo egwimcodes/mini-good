1c2bfb7d6747adb7fbd8ae2aeabc8d67224c97e0
egwimcodes <egwimcodes@gmail.com>
2024-06-15 11:24:19 +0100

    fixing

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -9,14 +9,6 @@ export async function GET(req: NextRequest) {
       data: { accessToken: req.cookies.get('accessToken') },
 
   });
-
-  // Set the cookie using NextResponse
-
-    response.cookies.set('accessToken', '', {
-    httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development',
-    sameSite: 'strict',
-    });
     
     response.cookies.get('accessToken');
   // Return the response with the cookie
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import HomePage from '@/components/Pages/HomePage';
 import _404 from '@/components/Pages/_404';
-// import { Register } from '@/utils/requests';
+import { Register } from '@/utils/requests';
 import { useWebApp } from '@vkruglikov/react-telegram-web-app';
 import React, { useState, useEffect } from 'react';
 
@@ -27,66 +27,66 @@ const Home = () => {
         }
 
         const data = await response.json();
-        if (data) {
+        if (JSON.stringify(data.data.accessToken.value) !== '') {
           // User is authenticated
           alert('User is authenticated now');
           alert(`${JSON.stringify(data.data.accessToken.value)}`);
 
         }
-        // else {
-        //   // User is not authenticated
-        //   try {
+        else {
+          // User is not authenticated
+          try {
 
-        //     const userData = webAppData.initDataUnsafe;
-        //     const userInfo = {
-        //       password: `${userData.user.id}`,
-        //       username: userData.user.username,
-        //       first_name: userData.user.first_name,
-        //       referral_code: userData.start_param ?? "",
-        //       is_premium_user: userData.user.is_premium_user
-        //         ?? false
-        //     };
-        //     Register(userInfo)
-        //       .then((e) => {
-        //         const storeData = async () => {
-        //           try {
-        //             // Ensure userData is a JSON string before storing
-        //             const dataToStore = typeof e === 'string' ? e : JSON.stringify(e);
-        //             const accessTokenToStore = JSON.parse(dataToStore).token.access;
+            const userData = webAppData.initDataUnsafe;
+            const userInfo = {
+              password: `${userData.user.id}`,
+              username: userData.user.username,
+              first_name: userData.user.first_name,
+              referral_code: userData.start_param ?? "",
+              is_premium_user: userData.user.is_premium_user
+                ?? false
+            };
+            Register(userInfo)
+              .then((e) => {
+                const storeData = async () => {
+                  try {
+                    // Ensure userData is a JSON string before storing
+                    const dataToStore = typeof e === 'string' ? e : JSON.stringify(e);
+                    const accessTokenToStore = JSON.parse(dataToStore).token.access;
 
-        //             // alert(`Registration Data ${dataToStore} `)
-        //              alert(`Registration accessToken ${accessTokenToStore} `)
-        //             alert('User is authenticated after registration');
-        //           } catch (error) {
-        //             alert(`Error storing data: ${error}`);
-        //           }
-        //         }
-        //         storeData();
-        //       })
-        //       .catch(
-        //         (e) => alert(`Error from Register ${JSON.stringify(e)}`)
-        //       );
+                    // alert(`Registration Data ${dataToStore} `)
+                     alert(`Registration accessToken ${accessTokenToStore} `)
+                    alert('User is authenticated after registration');
+                  } catch (error) {
+                    alert(`Error storing data: ${error}`);
+                  }
+                }
+                storeData();
+              })
+              .catch(
+                (e) => alert(`Error from Register ${JSON.stringify(e)}`)
+              );
 
-        //     //const accessTokenToStore = data.data.accessToken;
-        //     // await fetch('/api/your-route-name', {
-        //     //   method: 'POST',
-        //     //   headers: {
-        //     //     'Content-Type': 'application/json',
-        //     //   },
-        //     //   body: JSON.stringify(data),
-        //     // });
+            //const accessTokenToStore = data.data.accessToken;
+            // await fetch('/api/your-route-name', {
+            //   method: 'POST',
+            //   headers: {
+            //     'Content-Type': 'application/json',
+            //   },
+            //   body: JSON.stringify(data),
+            // });
 
-        //     // if (!response.ok) {
-        //     //   throw new Error('Network response was not ok');
-        //     // }
+            // if (!response.ok) {
+            //   throw new Error('Network response was not ok');
+            // }
 
-        //     // const result = await response.json();
-        //     // console.log('Response:', result);
+            // const result = await response.json();
+            // console.log('Response:', result);
 
-        //   } catch (error) {
-        //     console.error('Error posting data:', error);
-        //   }
-        // }
+          } catch (error) {
+            console.error('Error posting data:', error);
+          }
+        }
       } catch (error) {
         console.error('Error fetching data:', error);
       }
