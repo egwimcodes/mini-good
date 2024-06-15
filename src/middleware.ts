19f653d152a2845748de32ce17ff6e3202d9ac03
egwimcodes <egwimcodes@gmail.com>
2024-06-15 07:19:03 +0100

    fixing

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
     const response = NextResponse.next()
     const accessToken = response.cookies.get("accessToken")
+    console.log(req)
     if (!accessToken) {
         return null;
     }
