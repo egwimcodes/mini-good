bb87b92a3051ba590b4a5fc2e25e670ad1302e04
egwimcodes <egwimcodes@gmail.com>
2024-06-16 22:00:29 +0100

    fixing

--- src/components/Pages/Profile.tsx
+++ src/components/Pages/Profile.tsx
@@ -1,23 +1,23 @@
-// import { useUserContext } from "@/hooks/UserContext";
-import { useUserContext } from "@/hooks/UserContext";
-import Image from "next/image"
-import { MdNavigateNext } from "react-icons/md"
+import React from 'react';
+import Image from 'next/image';
+import { MdNavigateNext } from 'react-icons/md';
+import { useUserContext } from '@/hooks/UserContext';
+
 type ProfileProps = {
     setShowProfile: (value: string) => void;
 };
 
-export default function Profile({ setShowProfile }: ProfileProps) {
+const Profile: React.FC<ProfileProps> = ({ setShowProfile }) => {
     const user = useUserContext();
-    // const user = useUserContext();
+
     return (
-        <div className='w-[100vw] h-[90vh] text-light flex-col ' onClick={() => setShowProfile("profile")}>
+        <div className='w-[100vw] h-[90vh] text-light flex-col' onClick={() => setShowProfile("profile")}>
             <div className="profile-content w-[95vw] h-[30vh] flex-col flex-center">
-                <div className="avater-profile w-[20vw] border-2 border-main rounded-[50%]">
+                <div className="avatar-profile w-[20vw] border-2 border-main rounded-[50%]">
                     <Image src="/robot.png" width={100} height={100} alt="" />
                 </div>
                 <p className='text-3xl font-bold'>{user.username}</p>
                 <div className="children-body xsm:flex-2 flex-col items-center justify-center w-full">
-                    {/* <ChargeLevel level={100} availableGoodCoin={100} chargedGoodCoin={100} /> */}
                     <div className="level flex flex-row items-center justify-center flex-nowrap">
                         <Image className="w-8" width={100} height={100} src='/regendcup.png' draggable="false" alt="" />
                         <p className="text-base text-gray font-bold text-main mx-1">{user.rank}</p>
@@ -27,30 +27,31 @@ export default function Profile({ setShowProfile }: ProfileProps) {
             </div>
             <hr className="w-[95vw] border-1 border-main mx-auto" />
             <div className="profile-content w-[95vw] h-[60vh] flex-col flex-center">
-                <div className="profile-contnt-items flex-col flex-center">
+                <div className="profile-content-items flex-col flex-center">
                     <h4 className="text-1xl font-bold">Daily Earners</h4>
-                    <div className="children-body flex flex-row items-center justify-center w-full" >
+                    <div className="children-body flex flex-row items-center justify-center w-full">
                         <Image className="w-5" width={50} height={50} src='/goodcoing.png' draggable="false" alt="" />
-                        <p className="text-base text-gray font-bold text-main mx-1">900 MIllion</p>
+                        <p className="text-base text-gray font-bold text-main mx-1">900 Million</p>
                     </div>
                 </div>
                 <hr className="w-[70vw] border-1 my-3 border-main mx-auto" />
-                <div className="profile-contnt-items flex-col flex-center">
+                <div className="profile-content-items flex-col flex-center">
                     <h4 className="text-1xl font-bold">Active</h4>
-                    <div className="children-body flex flex-row items-center justify-center w-full" >
+                    <div className="children-body flex flex-row items-center justify-center w-full">
                         <p className="text-base text-gray font-bold text-main mx-1">2455675</p>
                     </div>
                 </div>
                 <hr className="w-[50vw] border-1 my-3 border-main mx-auto" />
-                <div className="profile-contnt-items flex-col flex-center">
+                <div className="profile-content-items flex-col flex-center">
                     <h4 className="text-1xl font-bold">All Taps</h4>
-                    <div className="children-body flex flex-row items-center justify-center w-full" >
+                    <div className="children-body flex flex-row items-center justify-center w-full">
                         <p className="text-base text-gray font-bold text-main mx-1">100M</p>
                     </div>
                 </div>
                 <hr className="w-[30vw] border-1 my-3 border-main mx-auto" />
-
             </div>
         </div>
-    )
-}
+    );
+};
+
+export default Profile;
