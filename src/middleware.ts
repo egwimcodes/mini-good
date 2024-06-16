18688ef6283f6b9b6fdb22b11e846f8aa4724eb6
egwimcodes <egwimcodes@gmail.com>
2024-06-16 15:22:29 +0100

    fixing

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,10 @@ export async function middleware(request: NextRequest) {
   const token = request.cookies.get('accessToken');
 
   if (token) {
+    alert(`accessToken cookie3 ${token} `)
     const response = NextResponse.next();
     response.cookies.delete('accessToken');
+    alert(`accessToken cookie4 ${response.cookies.get('accessToken')} `)
     return response;
   }
   // } else {
