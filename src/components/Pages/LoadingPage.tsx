72c708f6a1cc10cacce9b089673f27a531d24d9a
egwimcodes <egwimcodes@gmail.com>
2024-06-17 00:06:53 +0100

    fixing

--- src/components/Pages/LoadingPage.tsx
+++ src/components/Pages/LoadingPage.tsx
@@ -12,14 +12,14 @@ export default function LoadingPage() {
 
   return (
     <>
-      <div className="w-full h-screen flex flex-col items-center justify-center relative">
+      <div className="w-full h-screen flex flex-col items-center justify-center relative bg-gradient-to-tl from-gray-800">
         <Image className='coin-svg w-[30%]' src="/goodcoing.png" alt="" width={100} height={100} />
         <div className="absolute">
           <div className="loader "></div>
 
         </div>
         <div className='absolute bottom-10  w-[100%] h-[20%] flex-center'>
-          <h1 className='text-main text-2xl font-semibold text-shadow'>{text}</h1>
+          <h1 className='text-cyan-200 text-2xl font-semibold text-shadow'>{text}</h1>
         </div>
 
       </div>
