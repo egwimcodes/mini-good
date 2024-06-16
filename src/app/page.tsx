91e09295234b8253d7955b2b6c361019733f4ca9
egwimcodes <egwimcodes@gmail.com>
2024-06-16 21:49:55 +0100

    fixing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,42 +2,27 @@
 
 import HomePage from "@/components/Pages/HomePage";
 import _404 from "@/components/Pages/_404";
+import { Login, Register, RetriveMe } from "@/utils/requests";
 import { useWebApp } from "@vkruglikov/react-telegram-web-app";
 import React, { useState, useEffect } from "react";
+import { fetchAccessToken, setAccessToken } from "@/utils/api";
 import LoadingPage from "@/components/Pages/LoadingPage";
 import { UserContext } from "@/hooks/UserContext";
 import { UserData } from "@/types";
-import { fetchAccessToken, setAccessToken } from "@/utils/api"; // Adjust the imports as necessary
-import { Login, Register, RetriveMe } from "@/utils/requests";
 
 const Home = () => {
   const webAppData = useWebApp();
   const [show404, setShow404] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-  const [user, setUser] = useState<UserData | null>(null);
+  const [user, setUser] = useState<UserData | null>(null); // Initialize user state as null or undefined
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetchAccessToken();
-        const userData = response.data; // Adjust based on your actual data structure
-        setUser(userData);
-        setIsLoading(false);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        setIsLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    const userData = webAppData.initDataUnsafe;
-    const getUserAuth = async () => {
-      try {
-        const response = await fetchAccessToken();
         if (response.data.accessToken.value === "") {
           try {
+            const userData = webAppData.initDataUnsafe;
             const userInfo = {
               password: `${userData.user.id}`,
               username: userData.user.username,
@@ -45,117 +30,123 @@ const Home = () => {
               referral_code: userData.start_param ?? "",
               is_premium_user: userData.user.is_premium_user ?? false,
             };
+
             Register(userInfo)
-              .then((e) => {
-                const storeData = async () => {
-                  try {
-                    const dataToStore = typeof e === "string" ? e : JSON.stringify(e);
-                    const accessTokenToStore = JSON.parse(dataToStore).token.access;
-                    await setAccessToken(accessTokenToStore);
-                    RetriveMe()
-                      .then((e) => {
-                        setUser(e);
-                        setIsLoading(false);
-                      })
-                      .catch((e) => {
-                        console.error("Error when retrieving me:", e);
-                      });
-                  } catch (error) {
-                    alert(`Error storing data on register: ${error}`);
-                  }
-                };
-                storeData();
+              .then(async (e) => {
+                const dataToStore =
+                  typeof e === "string" ? e : JSON.stringify(e);
+                const accessTokenToStore =
+                  JSON.parse(dataToStore).token.access;
+                await setAccessToken(accessTokenToStore);
+
+                // Retrieve user data after registration
+                RetriveMe()
+                  .then((e) => {
+                    setUser(e);
+                    setIsLoading(false);
+                  })
+                  .catch((e) => {
+                    console.error("Error when retrieving me:", e);
+                    setIsLoading(false); // Handle error and stop loading
+                  });
               })
-              .catch((e) => alert(`Error from Register ${JSON.stringify(e)}`));
+              .catch((e) => {
+                console.error("Error from Register:", e);
+                setIsLoading(false); // Handle error and stop loading
+              });
           } catch (error) {
             console.error("Error from Register || login:", error);
+            setIsLoading(false); // Handle error and stop loading
           }
         } else {
-          const storeToken = async () => {
-            const userLoginInfo = {
-              username: userData.user.username,
-              password: `${userData.user.id}`,
-            };
-            try {
-              Login(userLoginInfo)
-                .then((e) => {
-                  if (e.name === "AxiosError") {
-                    try {
-                      const userInfo = {
-                        password: `${userData.user.id}`,
-                        username: userData.user.username,
-                        first_name: userData.user.first_name,
-                        referral_code: userData.start_param ?? "",
-                        is_premium_user: userData.user.is_premium_user ?? false,
-                      };
-                      Register(userInfo)
-                        .then((e) => {
-                          const storeData = async () => {
-                            try {
-                              const dataToStore = typeof e === "string" ? e : JSON.stringify(e);
-                              const accessTokenToStore = JSON.parse(dataToStore).token.access;
-                              await setAccessToken(accessTokenToStore);
-                              RetriveMe()
-                                .then((e) => {
-                                  setUser(e);
-                                  setIsLoading(false);
-                                })
-                                .catch((e) => {
-                                  console.error("Error when retrieving me after login fail:", e);
-                                });
-                            } catch (error) {
-                              alert(`Error storing data on register after login fail: ${error}`);
-                            }
-                          };
-                          storeData();
-                        })
-                        .catch((e) => alert(`Error from Register ${JSON.stringify(e)}`));
-                    } catch (error) {
-                      console.error("Error from Register || login after login fail:", error);
-                    }
-                  } else {
-                    const storeDataFunc = async () => {
-                      await setAccessToken(e.access);
+          const userData = webAppData.initDataUnsafe;
+          const userLoginInfo = {
+            username: userData.user.username,
+            password: `${userData.user.id}`,
+          };
+
+          Login(userLoginInfo)
+            .then(async (e) => {
+              if (e.name === "AxiosError") {
+                try {
+                  const userInfo = {
+                    password: `${userData.user.id}`,
+                    username: userData.user.username,
+                    first_name: userData.user.first_name,
+                    referral_code: userData.start_param ?? "",
+                    is_premium_user: userData.user.is_premium_user ?? false,
+                  };
+
+                  Register(userInfo)
+                    .then(async (e) => {
+                      const dataToStore =
+                        typeof e === "string" ? e : JSON.stringify(e);
+                      const accessTokenToStore =
+                        JSON.parse(dataToStore).token.access;
+                      await setAccessToken(accessTokenToStore);
+
+                      // Retrieve user data after registration
                       RetriveMe()
-                        .then((res) => {
-                          setUser(res);
+                        .then((e) => {
+                          setUser(e);
                           setIsLoading(false);
                         })
                         .catch((e) => {
-                          console.error("Error when retrieving me:", e);
+                          console.error(
+                            "Error when retrieving me after login fail:",
+                            e
+                          );
+                          setIsLoading(false); // Handle error and stop loading
                         });
-                      setIsLoading(false);
-                    };
-                    storeDataFunc();
-                  }
-                })
-                .catch((e) => {
-                  alert(`Error from Login call ${JSON.stringify(e)}`);
-                });
-            } catch (error) {
-              alert(`Error from login this user: ${error}`);
-            }
-          };
-          storeToken();
+                    })
+                    .catch((e) => {
+                      console.error("Error from Register:", e);
+                      setIsLoading(false); // Handle error and stop loading
+                    });
+                } catch (error) {
+                  console.error("Error from Register || login:", error);
+                  setIsLoading(false); // Handle error and stop loading
+                }
+              } else {
+                await setAccessToken(e.access);
+
+                // Retrieve user data after successful login
+                RetriveMe()
+                  .then((res) => {
+                    setUser(res);
+                    setIsLoading(false);
+                  })
+                  .catch((e) => {
+                    console.error("Error when retrieving me:", e);
+                    setIsLoading(false); // Handle error and stop loading
+                  });
+              }
+            })
+            .catch((e) => {
+              console.error("Error from Login call:", e);
+              setIsLoading(false); // Handle error and stop loading
+            });
         }
       } catch (error) {
-        alert(`Error fetching login data in App: ${error}`);
+        console.error("Error fetching login data in App:", error);
+        setIsLoading(false); // Handle error and stop loading
       }
     };
-    getUserAuth();
-  }, [webAppData]);
+
+    if (!user) {
+      fetchData(); // Fetch data only if user is not already set
+    }
+  }, [user, webAppData]); // Dependency array should include webAppData to ensure useEffect is triggered when webAppData changes
 
   useEffect(() => {
     if (webAppData) {
       if (
         webAppData.platform &&
-        (webAppData.platform === "unknown" || webAppData.platform === "tdesktop")
+        (webAppData.platform === "unknown" ||
+          webAppData.platform === "tdesktop")
       ) {
         setShow404(true);
-        return;
-      }
-
-      if (webAppData.platform) {
+      } else {
         webAppData.expand();
         const userData = webAppData.initDataUnsafe;
         const userInfo = {
@@ -168,7 +159,7 @@ const Home = () => {
         console.log(userInfo);
       }
     }
-  }, [webAppData]);
+  }, [webAppData]); // Include webAppData in dependency array
 
   useEffect(() => {
     const handleContextMenu = (event: MouseEvent) => {
@@ -182,29 +173,20 @@ const Home = () => {
     };
   }, []);
 
+  if (show404) {
+    return <_404 />;
+  }
+
+  if (isLoading || !user) {
+    return <LoadingPage />;
+  }
+
+  // Render HomePage only when user data is loaded
   return (
-    <>
-      {show404 ? (
-        <_404 />
-      ) : (
-        webAppData?.platform &&
-        (webAppData.platform === "android" || webAppData.platform === "ios") &&
-        webAppData.initDataUnsafe &&
-        (isLoading ? (
-          <LoadingPage />
-        ) : (
-          user ? (
-            <UserContext.Provider value={user}>
-              <HomePage />
-            </UserContext.Provider>
-          ) : (
-            <LoadingPage />
-          )
-        ))
-      )}
-    </>
+    <UserContext.Provider value={user}>
+      <HomePage />
+    </UserContext.Provider>
   );
 };
-
 
 export default Home;
