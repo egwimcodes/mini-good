d27bb28f6c817fb70769d3f2ebd3b16b2c43cbde
egwimcodes <egwimcodes@gmail.com>
2024-06-16 11:13:44 +0100

    fixing

--- src/components/Pages/LoadingPage.tsx
+++ src/components/Pages/LoadingPage.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
-
+import Image from 'next/image'
 export default function LoadingPage() {
   return (
-      <>
-          <div className="w-full h-screen flex flex-col items-center justify-center">
-              <h1 className='text-4xl text-main'>Loading...</h1>
+    <>
+      <div className="w-full h-screen flex flex-col items-center justify-center relative">
+        <Image className='coin-svg' src="/goodcoing.png" alt="" width={100} height={100} />
+        <div className="absolute">
+          <div className="loader "></div>
+
+        </div>
       </div>
-      </>
+    </>
   )
 }
