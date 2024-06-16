47edbc2fc8d0e9b5b11b598b9d77f3aa4e6ba055
egwimcodes <egwimcodes@gmail.com>
2024-06-16 14:19:41 +0100

    fixing

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,15 +4,18 @@ export async function middleware(request: NextRequest) {
   const token = request.cookies.get('accessToken');
 
   if (token) {
-    return NextResponse.next();
-  } else {
     const response = NextResponse.next();
-    response.cookies.set('accessToken', '', {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      path: '/', // Set the path to ensure the cookie is accessible from the root
-    });
+    response.cookies.delete('accessToken');
     return response;
   }
+  // } else {
+  //   const response = NextResponse.next();
+  //   response.cookies.set('accessToken', '', {
+  //     httpOnly: true,
+  //     secure: process.env.NODE_ENV !== 'development',
+  //     sameSite: 'strict',
+  //     path: '/', // Set the path to ensure the cookie is accessible from the root
+  //   });
+  //   return response;
+  // }
 }
