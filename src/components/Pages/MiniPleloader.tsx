69d28cca1c0b6b0e093cebbf69b02965abe812cf
egwimcodes <egwimcodes@gmail.com>
2024-06-17 17:12:55 +0100

    fixing

---
+++ src/components/Pages/MiniPleloader.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MiniPleloader() {
+  return (
+      <div className="flex items-center justify-center h-screen absolute top-0 left-0 w-full mini-preloder">
+          <div className="mini-loader"></div>
+      </div>
+
+  )
+}
--- src/components/Pages/Task.tsx
+++ src/components/Pages/Task.tsx
@@ -3,6 +3,7 @@ import { RetriveTasks } from "@/utils/requests";
 import { MdNavigateNext } from "react-icons/md";
 import PopUpComfirmationTask from "@/components/PopUpComfirmation";
 import Image from "next/image";
+import MiniPleloader from "./MiniPleloader";
 
 interface Task {
     id: number;
@@ -17,6 +18,7 @@ export default function Task() {
     const [isClaiming, setIsClaiming] = useState(false);
     const [retrievedTasks, setRetrievedTasks] = useState<Task[]>([]);
     const [selectedTask, setSelectedTask] = useState<Task | undefined>(undefined);
+    const [stillFetching, setStillFetching] = useState<boolean>(true);
 
     useEffect(() => {
         RetriveTasks()
@@ -24,6 +26,7 @@ export default function Task() {
             .catch(() => {
                 alert('Error while fetching tasks');
             });
+        setStillFetching(false);
     }, []);
 
     const selectedTaskFunc = (task: Task) => {
@@ -33,40 +36,44 @@ export default function Task() {
 
     return (
         <>
-            <div className="task-container w-full h-full overflow-y-scroll scroll-smooth bg-transparent mt-4 bg-slate-800">
-                <div className="boost-btn-container h-full w-full flex flex-col items-center bg-transparent">
-                    {retrievedTasks.map((task) => (
-                        <div
-                            key={task.id}
-                            className="task-claim min-w-[95%] h-13% flex flex-row items-center justify-between px-2 py-2 mb-1 cursor-pointer"
-                            onClick={() => selectedTaskFunc(task)}
-                        >
-                            <div className="boost-writeup flex flex-row items-center justify-center">
-                                <div className="icon-container">
-                                    <Image className="w-10" draggable={false} width={50} height={50} src={task.avatar} alt={task.title} />
-                                </div>
-                                <div className="task-writeup ml-3 flex flex-col justify-between">
-                                    <h3 className="text-light font-semibold">{task.title}</h3>
-                                    <div className="coin-task-container flex flex-row items-center">
-                                        <Image className="w-5" draggable={false} width={50} height={50} src="/good-coin.png" alt="Reward" />
-                                        <h3 className="text-main text-sm ml-1">+{task.reward}</h3>
+            {stillFetching ?
+                <MiniPleloader />
+                : <div className="task-container w-full h-full overflow-y-scroll scroll-smooth bg-transparent mt-4 bg-slate-800">
+                    <div className="boost-btn-container h-full w-full flex flex-col items-center bg-transparent">
+                        {retrievedTasks.map((task) => (
+                            <div
+                                key={task.id}
+                                className="task-claim min-w-[95%] h-13% flex flex-row items-center justify-between px-2 py-2 mb-1 cursor-pointer"
+                                onClick={() => selectedTaskFunc(task)}
+                            >
+                                <div className="boost-writeup flex flex-row items-center justify-center">
+                                    <div className="icon-container">
+                                        <Image className="w-10" draggable={false} width={50} height={50} src={task.avatar} alt={task.title} />
+                                    </div>
+                                    <div className="task-writeup ml-3 flex flex-col justify-between">
+                                        <h3 className="text-light font-semibold">{task.title}</h3>
+                                        <div className="coin-task-container flex flex-row items-center">
+                                            <Image className="w-5" draggable={false} width={50} height={50} src="/good-coin.png" alt="Reward" />
+                                            <h3 className="text-main text-sm ml-1">+{task.reward}</h3>
+                                        </div>
                                     </div>
                                 </div>
+                                <MdNavigateNext className="r-arrow text-2xl font-bold" />
                             </div>
-                            <MdNavigateNext className="r-arrow text-2xl font-bold" />
-                        </div>
-                    ))}
+                        ))}
+                    </div>
+                    {isClaiming && selectedTask && (
+                        <PopUpComfirmationTask
+                            isClose={() => setIsClaiming(false)}
+                            isopen={true}
+                            title={selectedTask.title}
+                            avatar={selectedTask.avatar}
+                            reward={selectedTask.reward}
+                            content={"Are you sure you want to claim this task?"}
+                        />
+                    )}
                 </div>
-            </div>
-            {isClaiming && selectedTask && (
-                <PopUpComfirmationTask
-                    isClose={() => setIsClaiming(false)}
-                    isopen={true}
-                    title={selectedTask.title}
-                    avatar={selectedTask.avatar}
-                    reward={selectedTask.reward}
-                />
-            )}
+            }
         </>
     );
 }
