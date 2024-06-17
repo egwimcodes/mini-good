a14aff5ba1e6662c87eb7594e56ef30933949b4f
egwimcodes <egwimcodes@gmail.com>
2024-06-17 22:29:38 +0100

    fixing

--- src/components/Pages/Referral.tsx
+++ src/components/Pages/Referral.tsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import { RetriveReferrals } from '@/utils/requests'
 import { useUserContext } from '@/hooks/UserContext'
