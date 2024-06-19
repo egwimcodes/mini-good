ebcd59f1e87d5362e87c236c89ee039123f829dc
egwimcodes <egwimcodes@gmail.com>
2024-06-19 08:37:01 +0100

    fixing

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,21 +1,21 @@
-// utils/dateUtils.ts
 export const isStreakContinued = (lastCheckinDate: string): boolean => {
     const lastCheckin = new Date(lastCheckinDate);
     const today = new Date();
 
-    // Set the time part to 0 to only compare dates
     lastCheckin.setHours(0, 0, 0, 0);
     today.setHours(0, 0, 0, 0);
 
     const differenceInTime = today.getTime() - lastCheckin.getTime();
     const differenceInDays = differenceInTime / (1000 * 60 * 60 * 24);
 
-    return differenceInDays <= 1;  // Adjust the condition based on your requirement
+    return differenceInDays <= 1; // Adjust the condition based on your requirement
 };
 
 export const isWithin24Hours = (lastCheckinDate: string): boolean => {
     const lastCheckin = new Date(lastCheckinDate);
     const now = new Date();
-    const timeDifference = now.getTime() - lastCheckin.getTime();
-    return timeDifference < 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+
+    const differenceInTime = now.getTime() - lastCheckin.getTime();
+
+    return differenceInTime < (24 * 60 * 60 * 1000); // 24 hours in milliseconds
 };
