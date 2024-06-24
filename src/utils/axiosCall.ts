416fe055593a0b9024690a6adb3bec6f6924a373
egwimcodes <egwimcodes@gmail.com>
2024-06-24 13:27:37 +0100

    fixing

--- src/utils/axiosCall.ts
+++ src/utils/axiosCall.ts
@@ -1,5 +1,56 @@
+// /* eslint-disable no-useless-catch */
+// import Axios from "axios";
+// import { ApiRequestConfig } from "@/types";
+// import { fetchAccessToken } from "./api";
+
+// //const SERVER_HOST = "https://clownfish-app-lqqur.ondigitalocean.app";
+// const SERVER_HOST =  "http://195.35.1.248";
+// const SERVER_VERSION = "v1";
+// const baseURL: string = `${SERVER_HOST}`;
+
+// export async function MakeRequest(
+//     requestObj: ApiRequestConfig,
+//     config = {}
+// ) {
+
+//     // Destructure properties from requestObj
+//     try {
+//         const { path, data, method, contentType, removeAuth } = requestObj;
+
+//         let token: string | null = null;
+//         if (!removeAuth) {
+          
+//             const accessToken = await fetchAccessToken();
+//             const tokenData = accessToken.data.accessToken.value;
+//             token = tokenData !== null ? tokenData : null;
+//         }
+
+//         // Set up headers
+//         const headers = {
+//             Authorization: removeAuth ? "No Auth" : token ? `Bearer ${token}` : "No Auth",
+//             Accept: "application/json",
+//             "Content-Type": contentType ? contentType : "application/json;charset=UTF-8",
+//         };
+
+//         const url = `${baseURL}/api/${SERVER_VERSION}${path}`
+//         const response = await Axios({ url, method, headers, data, ...config });
+
+//         // Check response status and return result
+//         if (response.status === 200 || response.status === 201) {
+//             const result = response && response.data;
+//             return result;
+//         }else {
+//             throw new Error(`Request failed with status code ${response.status}`);
+//         }
+
+//     } catch (e) {
+//         throw e
+//     }
+// }
+
+
 /* eslint-disable no-useless-catch */
-import Axios from "axios";
+import axios from "axios";
 import { ApiRequestConfig } from "@/types";
 import { fetchAccessToken } from "./api";
 
@@ -14,40 +65,40 @@ export async function MakeRequest(
 ) {
 
     // Destructure properties from requestObj
+    const { path, data, method, contentType, removeAuth } = requestObj;
+
+    let token: string | null = null;
+    if (!removeAuth) {
+        const accessToken = await fetchAccessToken();
+        const tokenData = accessToken.data.accessToken.value;
+        token = tokenData !== null ? tokenData : null;
+    }
+
+    // Set up headers
+    const headers = {
+        Authorization: removeAuth ? "No Auth" : token ? `Bearer ${token}` : "No Auth",
+        Accept: "application/json",
+        "Content-Type": contentType ? contentType : "application/json;charset=UTF-8",
+    };
+
+    const url = `${baseURL}/api/${SERVER_VERSION}${path}`;
+
     try {
-        const { path, data, method, contentType, removeAuth } = requestObj;
-
-        let token: string | null = null;
-        if (!removeAuth) {
-          
-            const accessToken = await fetchAccessToken();
-            const tokenData = accessToken.data.accessToken.value;
-            token = tokenData !== null ? tokenData : null;
-        }
-
-        // Set up headers
-        const headers = {
-            Authorization: removeAuth ? "No Auth" : token ? `Bearer ${token}` : "No Auth",
-            Accept: "application/json",
-            "Content-Type": contentType ? contentType : "application/json;charset=UTF-8",
-        };
-
-        const url = `${baseURL}/api/${SERVER_VERSION}${path}`
-        const response = await Axios({ url, method, headers, data, ...config });
+        const response = await axios({
+            url,
+            method,
+            headers,
+            data,
+            ...config,
+        });
 
         // Check response status and return result
         if (response.status === 200 || response.status === 201) {
-            const result = response && response.data;
-            alert(`Result is: ${result} and status is: ${response.status}`);
-            alert(`String Result: ${JSON.stringify(result)}`);
-            return result;
-        }else {
+            return response.data;
+        } else {
             throw new Error(`Request failed with status code ${response.status}`);
         }
-
     } catch (e) {
-        throw e
+        throw e;
     }
-
-
 }
