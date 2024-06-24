e2a03dada26bc5ebf8fb12f1538525ff8824efa2
egwimcodes <egwimcodes@gmail.com>
2024-06-24 18:24:22 +0100

    fixing

--- src/utils/api.js
+++ src/utils/api.js
@@ -21,7 +21,7 @@ const fetchAccessToken = async () => {
 };
 
 
-const setAccessToken = async (accessTokenToStore: string) => {
+const setAccessToken = async (accessTokenToStore) => {
     // Store access token
     try {
         const res = await fetch('/api/login', {
