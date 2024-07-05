fd424376ce4311862dd9c1b417d6f54c8ee79bfd
egwimcodes <egwimcodes@gmail.com>
2024-07-05 03:00:32 +0100

    fixing

--- src/components/Pages/Dashboard.tsx
+++ src/components/Pages/Dashboard.tsx
@@ -188,7 +188,7 @@ export default function Dashboard({ token }: DashboardProps) {
                         <div className="tap w-[60%] relative">
                             <Image src="/goodcoing.svg" className="coin-svg mx-auto sm:w-[40%]" width={500} height={100} alt="" />
                             <div className="png-coin w-[100%] h-[100%] bg-red-800">
-                                <Image src="/goodcoing.png" className="coin-png shrink-on-click absolute sm:w-[40%]" width={500} height={100} alt="" onClick={handleImageClick} />
+                                <Image src="https://ik.imagekit.io/egwimcodes/goodcoing.png?updatedAt=1720197417578" className="coin-png shrink-on-click absolute sm:w-[40%]" width={500} height={100} alt="" onClick={handleImageClick} />
                                 {clickEffects.map(effect => (
                                     <span
                                         key={effect.id}
