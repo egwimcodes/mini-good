fe9064ba69742c931a116c64b44a79e9e01eaefa
egwimcodes <egwimcodes@gmail.com>
2024-07-07 08:44:38 +0100

    fixing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import _404 from "@/components/Pages/_404";
 import { Login, Register, RetriveMe } from "@/utils/requests";
 import { useWebApp } from "@vkruglikov/react-telegram-web-app";
 import React, { useState, useEffect } from "react";
-import { fetchAccessToken, setAccessToken, } from "@/utils/api";
+import { fetchAccessToken, setAccessToken } from "@/utils/api";
 import LoadingPage from "@/components/Pages/LoadingPage";
 import { UserContext } from "@/hooks/UserContext";
 import { UserData } from "@/types";
@@ -15,50 +15,58 @@ const Home = () => {
   const webAppData = useWebApp();
   const [show404, setShow404] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-  const [user, setUser] = useState<UserData | null>(null); // Initialize user state as null or 
+  const [user, setUser] = useState<UserData | null>(null); // Initialize user state as null or
   const [token, setToken] = useState<string>();
-  const { message } = useWebSocket('wss://api.goodcoin.tech/ws/balance/?token=' + token);
+  const { message } = useWebSocket(
+    "wss://api.goodcoin.tech/ws/balance/?token=" + token
+  );
 
 
-
+// Authentication Check  
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetchAccessToken();
       const userData = webAppData.initDataUnsafe;
-      const userInfo = {
-        password: `${userData.user.id}`,
-        username: userData.user.username,
-        first_name: userData.user.first_name,
-        referral_code: userData.start_param ?? "",
-        is_premium_user: userData.user.is_premium ?? false,
+
+
+      // Register user if accessToken is empty or expired
+      const registerFunc = async () => {
+        const userInfo = {
+          password: `${userData.user.id}`,
+          username: userData.user.username,
+          first_name: userData.user.first_name,
+          referral_code: userData.start_param ?? "",
+          is_premium_user: userData.user.is_premium ?? false,
+        };
+        Register(userInfo)
+          .then(async (e) => {
+            const dataToStore = typeof e === "string" ? e : JSON.stringify(e);
+            const accessTokenToStore = JSON.parse(dataToStore).token.access;
+            setToken(accessTokenToStore);
+            await setAccessToken(accessTokenToStore);
+
+            // Retrieve user data after registration
+            RetriveMe()
+              .then((e) => {
+                setUser(e);
+                setIsLoading(false);
+              })
+              .catch((e) => {
+                console.error("Error when retrieving me:", e);
+                setIsLoading(false); // Handle error and stop loading
+              });
+          })
+          .catch((e) => {
+            console.error("Error from Register:", e);
+            setIsLoading(false); // Handle error and stop loading
+          });
       };
+
+      //  Try to login if accessToken is empty
       try {
         if (response.data.accessToken.value === "") {
           try {
-            Register(userInfo)
-              .then(async (e) => {
-                const dataToStore =
-                  typeof e === "string" ? e : JSON.stringify(e);
-                const accessTokenToStore =
-                  JSON.parse(dataToStore).token.access;
-                setToken(accessTokenToStore);
-                await setAccessToken(accessTokenToStore);
-
-                // Retrieve user data after registration
-                RetriveMe()
-                  .then((e) => {
-                    setUser(e);
-                    setIsLoading(false);
-                  })
-                  .catch((e) => {
-                    console.error("Error when retrieving me:", e);
-                    setIsLoading(false); // Handle error and stop loading
-                  });
-              })
-              .catch((e) => {
-                console.error("Error from Register:", e);
-                setIsLoading(false); // Handle error and stop loading
-              });
+            registerFunc();
           } catch (error) {
             console.error("Error from Register || login:", error);
             setIsLoading(false); // Handle error and stop loading
@@ -72,33 +80,7 @@ const Home = () => {
             .then(async (e) => {
               if (e.name === "AxiosError") {
                 try {
-                  Register(userInfo)
-                    .then(async (e) => {
-                      const dataToStore =
-                        typeof e === "string" ? e : JSON.stringify(e);
-                      const accessTokenToStore =
-                        JSON.parse(dataToStore).token.access;
-                      setToken(accessTokenToStore);
-                      await setAccessToken(accessTokenToStore);
-
-                      // Retrieve user data after registration
-                      RetriveMe()
-                        .then((e) => {
-                          setUser(e);
-                          setIsLoading(false);
-                        })
-                        .catch((e) => {
-                          console.error(
-                            "Error when retrieving me after login fail:",
-                            e
-                          );
-                          setIsLoading(false); // Handle error and stop loading
-                        });
-                    })
-                    .catch((e) => {
-                      console.error("Error from Register:", e);
-                      setIsLoading(false); // Handle error and stop loading
-                    });
+                  registerFunc();
                 } catch (error) {
                   console.error("Error from Register || login:", error);
                   setIsLoading(false); // Handle error and stop loading
@@ -160,12 +142,12 @@ const Home = () => {
   useEffect(() => {
     if (message) {
       const updatedBalanceParsed = JSON.parse(message);
-      setUser(prevUser => {
+      setUser((prevUser) => {
         if (!prevUser) return prevUser; // Handle the case where prevUser is null or undefined
         return {
           ...prevUser,
           balance: updatedBalanceParsed.balance,
-          tap_energy: updatedBalanceParsed.tap_energy
+          tap_energy: updatedBalanceParsed.tap_energy,
         };
       });
     }
@@ -193,7 +175,6 @@ const Home = () => {
 
   // Render HomePage only when user data is loaded
   return (
-
     <UserContext.Provider value={user}>
       <HomePage token={token ? token : ""} />
     </UserContext.Provider>
