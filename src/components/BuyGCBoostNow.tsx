9ed762667b473aea8a41019e01ca4ad7f8071378
egwimcodes <egwimcodes@gmail.com>
2024-07-08 10:59:57 +0100

    fixing

--- src/components/BuyGCBoostNow.tsx
+++ src/components/BuyGCBoostNow.tsx
@@ -27,12 +27,13 @@ export default function BuyBoostNow({ isopen, isClose, balance }: BuyBoostNowPro
                               <h1 className="text-light xxxsm:text-xs xxsm:text-2xl xsm:text-0.5rem sm:text-1rem font-semibold w-[80%]">BOOST</h1>
                               <IoCloseSharp className="text-main text-3xl w-[20%] " onClick={() => { isClose && isClose() }} />
                           </div>
-                          <h1 className="text-main xxxsm:text-xxxs xxsm:text-xs xsm:text-xs sm:text-xs  font-semibold">Balnce: {balance} GoodCoin</h1>
+                          <h1 className="text-main xxxsm:text-xxxs xxsm:text-xs xsm:text-xs sm:text-xs  font-semibold">Balance: {balance} GoodCoin</h1>
                           <Image src="https://ik.imagekit.io/egwimcodes/boost_battery.png?updatedAt=1720353194317" width={100} height={100} alt="" />
 
                           <div className="claim-guild text-center">
                               {btnText ? (<div>
-                                  <h4 className="text-main xxxsm:text-xxxs xxsm:text-xs xsm:text-xs sm:text-xs font-semibold">Successfull</h4>
+                                  <h4 className="text-main xxxsm:text-xxxs xxsm:text-xs xsm:text-xs sm:text-xs font-semibold">Successfully Purchased</h4>
+                                  <h4 className="text-main xxxsm:text-xxxs xxsm:text-xs xsm:text-xs sm:text-xs font-semibold">Boost</h4>
                               </div>) : (<div>
                                       <h4 className="text-orange-400 xxxsm:text-xxxs xxsm:text-xs xsm:text-xs sm:text-xs font-semibold">pending...</h4>
                                       {balance && balance >= 5000 ? (
