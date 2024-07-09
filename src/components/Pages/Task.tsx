49cbe047b528d5481ff0171723388604641b1b7a
egwimcodes <egwimcodes@gmail.com>
2024-07-09 14:43:25 +0100

    first production stage

--- src/components/Pages/Task.tsx
+++ src/components/Pages/Task.tsx
@@ -1,9 +1,9 @@
-// import { useEffect, useState } from "react";
-// import { RetriveTasks } from "@/utils/requests";
-// import { MdNavigateNext } from "react-icons/md";
-// import PopUpComfirmationTask from "@/components/PopUpComfirmation";
-// import Image from "next/image";
-// import MiniPreloader from "./MiniPleloader";
+ import { useEffect, useState } from "react";
+import { RetriveTasks } from "@/utils/requests";
+import { MdNavigateNext } from "react-icons/md";
+//import PopUpComfirmationTask from "@/components/PopUpComfirmation";
+import Image from "next/image";
+//import MiniPreloader from "./MiniPleloader";
 
 interface Task {
     id: number;
@@ -16,39 +16,43 @@ interface Task {
 
 export default function Task() {
     // const [isClaiming, setIsClaiming] = useState(false);
-    // const [retrievedTasks, setRetrievedTasks] = useState<Task[]>([]);
+     const [retrievedTasks, setRetrievedTasks] = useState<Task[]>([]);
     // const [selectedTask, setSelectedTask] = useState<Task | undefined>(undefined);
     // const [stillFetching, setStillFetching] = useState<boolean>(true);
 
-    // useEffect(() => {
-    //     RetriveTasks()
-    //         .then((tasks) => {
-    //             setRetrievedTasks(tasks);
-    //             setStillFetching(false); // Set fetching to false after tasks are retrieved
-    //         })
-    //         .catch(() => {
-    //             console.log('Error while fetching tasks');
-    //             setStillFetching(false); // Set fetching to false in case of an error
-    //         });
-    // }, []);
+    useEffect(() => {
+        RetriveTasks()
+            .then((tasks) => {
+                setRetrievedTasks(tasks);
+                alert(`Retrieved ${JSON.stringify(tasks)} tasks`);
+               // setStillFetching(false); // Set fetching to false after tasks are retrieved
+            })
+            .catch(() => {
+                console.log('Error while fetching tasks');
+                //setStillFetching(false); // Set fetching to false in case of an error
+            });
+    }, []);
 
-    // if (stillFetching) return <MiniPreloader />;
+  //  if (stillFetching) return <MiniPreloader />;
 
     // const selectedTaskFunc = (task: Task) => {
     //     setSelectedTask(task);
     //     setIsClaiming(true);
     // };
 
+    useEffect(() => {
+        alert(retrievedTasks)
+    }, [retrievedTasks])
     return (
         <>
             <div className="task-container w-full h-full overflow-y-scroll scroll-smooth bg-transparent mt-4 bg-slate-800">
                 <div className="boost-btn-container h-full w-full flex flex-col items-center bg-transparent">
                     <h1>Hi</h1>
-                    {/* {retrievedTasks.map((task) => (
+                    {retrievedTasks.map((task) => (
                         <div
                             key={task.id}
                             className="task-claim min-w-[95%] h-13% flex flex-row items-center justify-between px-2 py-2 mb-1 cursor-pointer"
-                            onClick={() => selectedTaskFunc(task)}
+                            onClick={() => (console.log("clicked"))}
                         >
                             <div className="boost-writeup flex flex-row items-center justify-center">
                                 <div className="icon-container">
@@ -64,7 +68,7 @@ export default function Task() {
                             </div>
                             <MdNavigateNext className="r-arrow text-2xl font-bold" />
                         </div>
-                    ))} */}
+                    ))}
                 </div>
                 {/* {isClaiming && selectedTask && (
                     <PopUpComfirmationTask
