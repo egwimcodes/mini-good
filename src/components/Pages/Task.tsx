547aa317f5def4eaad9dd651d98b5cb784aad422
egwimcodes <egwimcodes@gmail.com>
2024-07-09 15:02:00 +0100

    first production stage

--- src/components/Pages/Task.tsx
+++ src/components/Pages/Task.tsx
@@ -15,57 +15,68 @@ interface Task {
 }
 
 export default function Task() {
-    const [isClaiming, setIsClaiming] = useState(false);
+   // const [isClaiming, setIsClaiming] = useState(false);
     const [retrievedTasks, setRetrievedTasks] = useState<Task[]>([]);
-    const [selectedTask, setSelectedTask] = useState<Task | undefined>(undefined);
-    const [stillFetching, setStillFetching] = useState<boolean>(true);
+    //const [selectedTask, setSelectedTask] = useState<Task | undefined>(undefined);
+   // const [stillFetching, setStillFetching] = useState<boolean>(true);
 
     useEffect(() => {
-        RetriveTasks()
-            .then((tasks) => {
-                setRetrievedTasks(tasks);
-                setStillFetching(false); // Set fetching to false after tasks are retrieved
-            })
-            .catch(() => {
-                console.log('Error while fetching tasks');
-                setStillFetching(false); // Set fetching to false in case of an error
-            });
-    }, []);
+       async function fetchData() {
+           RetriveTasks()
+               .then((tasks) => {
+                   setRetrievedTasks(tasks);
+                  // setStillFetching(false); // Set fetching to false after tasks are retrieved
+               })
+               .catch(() => {
+                   alert('Error while fetching tasks');
+                 //  setStillFetching(false); // Set fetching to false in case of an error
+               });
+        }
 
-    if (stillFetching) return <MiniPreloader />;
+        // Fetch data initially
+        fetchData();
+    });
 
-    const selectedTaskFunc = (task: Task) => {
-        setSelectedTask(task);
-        setIsClaiming(true);
-    };
+    // if (stillFetching) return <MiniPreloader />;
+
+    // const selectedTaskFunc = (task: Task) => {
+    //     setSelectedTask(task);
+    //     setIsClaiming(true);
+    // };
 
     return (
         <>
             <div className="task-container w-full h-full overflow-y-scroll scroll-smooth bg-transparent mt-4 bg-slate-800">
                 <div className="boost-btn-container h-full w-full flex flex-col items-center bg-transparent">
                     {retrievedTasks.map((task) => (
-                        <div
-                            key={task.id}
-                            className="task-claim min-w-[95%] h-13% flex flex-row items-center justify-between px-2 py-2 mb-1 cursor-pointer"
-                            onClick={() => selectedTaskFunc(task)}
-                        >
-                            <div className="boost-writeup flex flex-row items-center justify-center">
-                                <div className="icon-container">
-                                    <Image className="w-10" draggable={false} width={50} height={50} src={task.avater} alt={task.title} />
-                                </div>
-                                <div className="task-writeup ml-3 min-w-[60%] flex flex-col justify-between">
-                                    <h3 className="text-light font-semibold">{task.title}</h3>
-                                    <div className="coin-task-container flex flex-row items-center">
-                                        <Image className="w-5" draggable={false} width={50} height={50} src="/good-coin.png" alt="Reward" />
-                                        <h3 className="text-main text-sm ml-1">+{task.reward}</h3>
-                                    </div>
-                                </div>
-                            </div>
-                            <MdNavigateNext className="r-arrow text-2xl font-bold" />
-                        </div>
-                    ))}
+                        <li key={task.id}>{JSON.stringify(task)} </li>
+                    )
+                    
+                    // (
+                    //     <div
+                    //         key={task.id}
+                    //         className="task-claim min-w-[95%] h-13% flex flex-row items-center justify-between px-2 py-2 mb-1 cursor-pointer"
+                    //         // onClick={() => selectedTaskFunc(task)}
+                    //     >
+                    //         <div className="boost-writeup flex flex-row items-center justify-center">
+                    //             <div className="icon-container">
+                    //                 <Image className="w-10" draggable={false} width={50} height={50} src={task.avater} alt={task.title} />
+                    //             </div>
+                    //             <div className="task-writeup ml-3 min-w-[60%] flex flex-col justify-between">
+                    //                 <h3 className="text-light font-semibold">{task.title}</h3>
+                    //                 <div className="coin-task-container flex flex-row items-center">
+                    //                     <Image className="w-5" draggable={false} width={50} height={50} src="/good-coin.png" alt="Reward" />
+                    //                     <h3 className="text-main text-sm ml-1">+{task.reward}</h3>
+                    //                 </div>
+                    //             </div>
+                    //         </div>
+                    //         <MdNavigateNext className="r-arrow text-2xl font-bold" />
+                    //     </div>
+                    // )
+                    
+                    )}
                 </div>
-                {isClaiming && selectedTask && (
+                {/* {isClaiming && selectedTask && (
                     <PopUpComfirmationTask
                         isClose={() => setIsClaiming(false)}
                         isopen={true}
@@ -75,7 +86,7 @@ export default function Task() {
                         task_url={selectedTask.task_url}
                         content={"Are you sure you want to claim this task?"}
                     />
-                )}
+                )} */}
             </div>
         </>
     );
