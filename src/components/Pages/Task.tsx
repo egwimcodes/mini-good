a3dc52204197a4e1569c1bc2e53f43d9dc459a6d
egwimcodes <egwimcodes@gmail.com>
2024-07-10 21:42:34 +0100

    first production stage

--- src/components/Pages/Task.tsx
+++ src/components/Pages/Task.tsx
@@ -4,10 +4,11 @@ import { MdNavigateNext } from "react-icons/md";
 import PopUpComfirmationTask from "@/components/PopUpComfirmation";
 import Image from "next/image";
 import MiniPreloader from "./MiniPleloader";
+import { useUserContext } from "@/hooks/UserContext";
 
 interface Task {
     id: number;
-    completed_users: []; // Adjust type if you know the exact type
+    completed_users: { user: { id: number } }[]; // Adjust type if you know the exact type
     title: string;
     reward: number;
     task_url: string;
@@ -15,6 +16,7 @@ interface Task {
 }
 
 export default function Task() {
+    const user_id = useUserContext();
     const [isClaiming, setIsClaiming] = useState(false);
     const [retrievedTasks, setRetrievedTasks] = useState<Task[]>([]);
     const [selectedTask, setSelectedTask] = useState<Task | undefined>(undefined);
@@ -43,29 +45,33 @@ export default function Task() {
         <>
             <div className="task-container w-full h-full overflow-y-scroll scroll-smooth bg-transparent mt-4 bg-slate-800">
                 <div className="boost-btn-container h-full w-full flex flex-col items-center bg-transparent">
-                    {retrievedTasks.map((task) => (
-                        <div
-                            key={task.id}
-                            className="task-claim min-w-[95%] h-13% flex flex-row items-center justify-between px-2 py-2 mb-1 cursor-pointer"
-                            onClick={() => selectedTaskFunc(task)}
-                        >
-                            <div className="boost-writeup flex flex-row items-center justify-center">
-                                <div className="icon-container">
-                                    {task.avater && (
-                                        <Image className="w-10" draggable={false} width={50} height={50} src={task.avater} alt={task.title} />
-                                    )}
-                                </div>
-                                <div className="task-writeup ml-3 min-w-[60%] flex flex-col justify-between">
-                                    <h3 className="text-light font-semibold">{task.title}</h3>
-                                    <div className="coin-task-container flex flex-row items-center">
-                                        <Image className="w-5" draggable={false} width={50} height={50} src="https://ik.imagekit.io/egwimcodes/goodcoing.png?updatedAt=1720197417578" alt="Reward" />
-                                        <h3 className="text-main text-sm ml-1">+{task.reward}</h3>
+                    {retrievedTasks.map((task) => {
+                        const userCompleted = task.completed_users.some((user) => String(user.user.id) === String(user_id.user_id));
+
+                        return userCompleted ? null : (
+                            <div
+                                key={task.id}
+                                className="task-claim min-w-[95%] h-13% flex flex-row items-center justify-between px-2 py-2 mb-1 cursor-pointer"
+                                onClick={() => selectedTaskFunc(task)}
+                            >
+                                <div className="boost-writeup flex flex-row items-center justify-center">
+                                    <div className="icon-container">
+                                        {task.avater && (
+                                            <Image className="w-10" draggable={false} width={50} height={50} src={task.avater} alt={task.title} />
+                                        )}
+                                    </div>
+                                    <div className="task-writeup ml-3 min-w-[60%] flex flex-col justify-between">
+                                        <h3 className="text-light font-semibold">{task.title}</h3>
+                                        <div className="coin-task-container flex flex-row items-center">
+                                            <Image className="w-5" draggable={false} width={50} height={50} src="https://ik.imagekit.io/egwimcodes/goodcoing.png?updatedAt=1720197417578" alt="Reward" />
+                                            <h3 className="text-main text-sm ml-1">+{task.reward}</h3>
+                                        </div>
                                     </div>
                                 </div>
+                                <MdNavigateNext className="r-arrow text-2xl font-bold" />
                             </div>
-                            <MdNavigateNext className="r-arrow text-2xl font-bold" />
-                        </div>
-                    ))}
+                        );
+                    })}
                 </div>
                 {isClaiming && selectedTask && (
                     <PopUpComfirmationTask
