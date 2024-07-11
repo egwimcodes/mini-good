49834b716d17cc3b54112a9035c77a356bcf3485
egwimcodes <egwimcodes@gmail.com>
2024-07-11 23:43:00 +0100

    first production stage

--- src/components/BuyGCBoostNow.tsx
+++ src/components/BuyGCBoostNow.tsx
@@ -20,43 +20,49 @@ export default function BuyBoostNow({ isopen, isClose, balance }: BuyBoostNowPro
             })
         }
     }
-  return (
-      <>
-          {
-              isopen && (
-                  <div className="absolute top-0 left-0 h-screen w-screen bg-black bg-opacity-50 flex items-center justify-center">
-                      <div className="claim-content h-[60vh] w-[90%] bg-black border-2 border-main rounded-[10px] flex flex-col items-center justify-evenly p-3 relative">
-                          <div className="claim-content-header w-[100%] flex flex-row items-center justify-between">
-                              <h1 className="text-light xxxsm:text-xs xxsm:text-2xl xsm:text-0.5rem sm:text-1rem font-semibold w-[80%]">BOOST</h1>
-                              <IoCloseSharp className="text-main text-3xl w-[20%] " onClick={() => { isClose && isClose() }} />
-                          </div>
-                          <h1 className="text-main xxxsm:text-xxxs xxsm:text-xs xsm:text-xs sm:text-xs  font-semibold">Balance: {balance} GoodCoin</h1>
-                          <Image src="https://ik.imagekit.io/egwimcodes/boost_battery.png?updatedAt=1720353194317" width={100} height={100} alt="" />
+    return (
+        <>
+            {
+                isopen && (
+                    <div className="absolute top-0 left-0 h-screen w-screen bg-black bg-opacity-50 flex items-center justify-center">
+                        <div className="claim-content h-[60vh] w-[90%] bg-black border-2 border-main rounded-[10px] flex flex-col items-center justify-evenly p-3 relative">
+                            <div className="claim-content-header w-[100%] flex flex-row items-center justify-between">
+                                <h1 className="text-light xxxsm:text-xs xxsm:text-2xl xsm:text-0.5rem sm:text-1rem font-semibold w-[80%]">BOOST</h1>
+                                <IoCloseSharp className="text-main text-3xl w-[20%] " onClick={() => { isClose && isClose() }} />
+                            </div>
+                            <h1 className="text-main xxxsm:text-xxxs xxsm:text-xs xsm:text-xs sm:text-xs  font-semibold">Balance: {balance} GoodCoin</h1>
+                            <Image src="https://ik.imagekit.io/egwimcodes/boost_battery.png?updatedAt=1720353194317" width={100} height={100} alt="" />
 
-                          <div className="claim-guild text-center">
-                              {btnText ? (<div>
-                                  <h4 className="text-main xxxsm:text-xxxs xxsm:text-xs xsm:text-xs sm:text-xs font-semibold">Successfully Purchased</h4>
-                                  <h4 className="text-main xxxsm:text-xxxs xxsm:text-xs xsm:text-xs sm:text-xs font-semibold">Boost</h4>
-                              </div>) : (<div>
-                                      <h4 className="text-orange-400 xxxsm:text-xxxs xxsm:text-xs xsm:text-xs sm:text-xs font-semibold">pending...</h4>
-                                      {balance && balance >= 5000 ? (
-                                          <h1 className="text-light xxxsm:text-xxxs xxsm:text-xs xsm:text-xs sm:text-xs text-main font-semibold">5000 GoodCoin Charge</h1>
-                                      ) : (
-                                              <div>
-                                                  <h1 className="text-light xxxsm:text-xxxs xxsm:text-xs xsm:text-xs sm:text-xs text-orange-400 font-semibold">Sorry you don't have enough GC</h1>
-                                                  
-                                          </div>
-                                      )}
-                              </div>)}
-                              
-                          </div>
-                          <div className={`claim-gift-btn w-[100%] bg-orange flex items-center justify-center h-[8vh] ${ balance && balance >= 5000 ? 'bg-main' : 'bg-red-700'} rounded-[10px] flex-evenly`} onClick={() => {balance && balance >= 5000 ? buyNow() : null}}>
-                              <h4 className="text-light font-semibold">{ btnText ? "Claimed" : "Buy Now"}</h4>
-                          </div>
-                      </div>
-                  </div>
-              )
-      }
-      </>
-  )
+                            <div className="claim-guild text-center">
+                                {btnText ? (<div>
+                                    <h4 className="text-main xxxsm:text-xxxs xxsm:text-xs xsm:text-xs sm:text-xs font-semibold">Successfully Purchased</h4>
+                                    <h4 className="text-main xxxsm:text-xxxs xxsm:text-xs xsm:text-xs sm:text-xs font-semibold">Boost</h4>
+                                </div>) : (<div>
+                                    <h4 className="text-orange-400 xxxsm:text-xxxs xxsm:text-xs xsm:text-xs sm:text-xs font-semibold">pending...</h4>
+                                    {balance && balance >= 5000 ? (
+                                        <h1 className="text-light xxxsm:text-xxxs xxsm:text-xs xsm:text-xs sm:text-xs text-main font-semibold">5000 GoodCoin Charge</h1>
+                                    ) : (
+                                        <div>
+                                            <h1 className="text-light xxxsm:text-xxxs xxsm:text-xs xsm:text-xs sm:text-xs text-orange-400 font-semibold">Sorry you don't have enough GC</h1>
+
+                                        </div>
+                                    )}
+                                </div>)}
+
+                            </div>
+                            {btnText ? (
+                                <div className={`claim-gift-btn w-[100%] bg-orange flex items-center justify-center h-[8vh] ${balance && balance >= 5000 ? 'bg-main' : 'bg-red-700'} rounded-[10px] flex-evenly`} onClick={() => { isClose && isClose() }}>
+                                    <h4 className="text-light font-semibold">{btnText ? "Close" : "Buy Now"}</h4>
+                                </div>
+                            ) : (
+                                <div className={`claim-gift-btn w-[100%] bg-orange flex items-center justify-center h-[8vh] ${balance && balance >= 5000 ? 'bg-main' : 'bg-red-700'} rounded-[10px] flex-evenly`} onClick={() => { balance && balance >= 5000 ? buyNow() : null }}>
+                                    <h4 className="text-light font-semibold">{btnText ? "Claimed" : "Buy Now"}</h4>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                )
+            }
+        </>
+    )
 }
