0da64d6ae5cf4cefaded43a6ce12421a0ff9848c
egwimcodes <egwimcodes@gmail.com>
2024-07-12 16:13:56 +0100

    first production stage

--- src/components/Pages/BuyBot_Boost.tsx
+++ src/components/Pages/BuyBot_Boost.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { MdNavigateNext } from "react-icons/md";
 import { useState } from "react";
 import BuyGCBoostNow from "../BuyGCBoostNow";
-import BuyUSDTBoostNow from "../BuyUSDTBoostNow";
+//import BuyUSDTBoostNow from "../BuyUSDTBoostNow";
 import { useUserContext } from "@/hooks/UserContext";
 interface PopUpComfirmationProps {
     isopen: boolean;
@@ -18,14 +18,14 @@ interface PopUpComfirmationProps {
 export default function BuyBot_Boost({ isopen, isClose }: PopUpComfirmationProps) {
     const user = useUserContext();
     const [buyGCNow, setBuyGCNow] = useState(false);
-    const [buyUSDTNow, setBuyUSDTNow] = useState(false);
+    // const [buyUSDTNow, setBuyUSDTNow] = useState(false);
     const handleBuyGCNow = () => {
         setBuyGCNow(true);
     }
-    const handleBuyUSDTNow = () => {
-        setBuyUSDTNow(true);
+    // const handleBuyUSDTNow = () => {
+    //     setBuyUSDTNow(true);
 
-    }
+    // }
     return (
         <>{
             isopen && (
@@ -49,14 +49,14 @@ export default function BuyBot_Boost({ isopen, isClose }: PopUpComfirmationProps
                             </div>
                             <MdNavigateNext className="text-2xl text-light font-bold" />
                         </div>
-                        <div className="claim-gift w-[100%] h-[20%] min-h[max-content] bg-emerald-700 flex items-center justify-evenly px-2 xsm:mb-2 rounded-[10px]" onClick={() => { handleBuyUSDTNow() }}>
+                        {/* <div className="claim-gift w-[100%] h-[20%] min-h[max-content] bg-emerald-700 flex items-center justify-evenly px-2 xsm:mb-2 rounded-[10px]" onClick={() => { handleBuyUSDTNow() }}>
                             <Image className="shine-coin w-10" width={50} height={50} src={"https://ik.imagekit.io/egwimcodes/usdt.png?updatedAt=1720353087479"} alt="" />
                             <div className="amount-to-claim ml-2 text-center">
                                 <p className="font-semibold text-white">USDT (USDt)</p>
                                 <h3 className="text-light xxxsm:text-xxxs xxsm:text-xs xsm:text-xs sm:text-xs text-orange-500 font-semibold">Buy with 1 USDT</h3>
                             </div>
                             <MdNavigateNext className="text-2xl text-light font-bold" />
-                        </div>
+                        </div> */}
 
 
                         {/* <div className="claim-gift-btn w-[100%] bg-orange flex items-center justify-center h-[8vh] bg-main rounded-[10px] flex-evenly">
@@ -71,9 +71,9 @@ export default function BuyBot_Boost({ isopen, isClose }: PopUpComfirmationProps
             {buyGCNow && (
                 <BuyGCBoostNow isopen={true} isClose={() => { isClose() }} balance={user?.balance} />
             )}
-            {buyUSDTNow && (
+            {/* {buyUSDTNow && (
                 <BuyUSDTBoostNow isopen={true} isClose={() => { isClose() }}/>
-            )}
+            )} */}
         </>
     )
 }
