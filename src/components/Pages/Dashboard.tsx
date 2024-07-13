eeb5a2479e3b9eec016b546e67c81a2882729eff
egwimcodes <egwimcodes@gmail.com>
2024-07-13 10:49:34 +0100

    first production stage

--- src/components/Pages/Dashboard.tsx
+++ src/components/Pages/Dashboard.tsx
@@ -52,7 +52,7 @@ export default function Dashboard({ sendMessage, message }: DashboardProps) {
         }, 2000);
 
         // Update taps and claimChange based on conditions
-        if (recivedCharges - earnPerTap >= earnPerTap  ) {
+        if (true) {
             const updatedTapEnergy = JSON.parse(`{"balance": ${earnPerTap},"tap_energy": ${- earnPerTap}}`);
             sendMessage(JSON.stringify(updatedTapEnergy));
             setRecivedCharges(prev => prev - earnPerTap);
@@ -123,7 +123,7 @@ export default function Dashboard({ sendMessage, message }: DashboardProps) {
                                         style={{ left: `${effect.x}px`, top: `${effect.y}px` }}
                                         draggable="false"
                                     >
-                                        {recivedCharges - earnPerTap >= earnPerTap && `+${earnPerTap}`}
+                                        {true &&  `+${earnPerTap}`}
                                     </span>
                                 ))}
                             </div>
